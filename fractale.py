03a6d3d66235258e8e1daf5e35854fce00eb2fbc
HFCItasi <mrpaperprhb@gmail.com>
2024-01-19 11:19:03 +0100

    up

--- fractale.py
+++ fractale.py
@@ -1,5 +1,5 @@
-f =str(input("Répertoire du fichier"))
-
+print ("veuillez fournir le fichier d'informations")
+f =str(input("Répertoire du fichier : "))
 
 def dictionnaire(f):
     dictionnaire_figures = {}
@@ -11,36 +11,30 @@ def dictionnaire(f):
             if len(elements) >= 2:
                 nom_forme = elements[0]
                 niveau_recursivite = elements[1]
-                autres_informations = elements[2:]
+                départ = elements[2]
+                taille = elements[3]
+                couleur = elements[4]
 
-                cle = f"{nom_forme}_{niveau_recursivite}"
-                valeur = autres_informations  
-
-                dictionnaire_figures[cle] = valeur
+                cle = f"{nom_forme} , {niveau_recursivite}" 
+                valeurs = [départ, taille, couleur]
+                dictionnaire_figures[cle] = valeurs
+            else: 
+                print("Veuillez fournir un fichier avec d'avantages d'information")
     return dictionnaire_figures
 
+ 
+def parcourir_dictionnaire(dictionnaire_figures):
+    for cle in dictionnaire_figures.keys():
+        valeurs = dictionnaire_figures[cle]
+        print(valeurs)
+        coords = [[0,1]] # calcul_coordonnées(valeurs)
+        fich = creation_fichiers(coords, cle)
+        dictionnaire_figures[cle] = fich
 
 
 
+d = dictionnaire(f)
+print(d)
 
 
 
-def calculer_coordonnees_fractale(nom_forme, niveau_recursivite, autres_informations):
-    print(f"Calcul des coordonnées pour {nom_forme} avec niveau de récursivité {niveau_recursivite}")
-    print("Autres informations:", autres_informations)
-    print("\n")
-
-def parcourir_dictionnaire(d):
-    for cle in d.keys():
-        # Séparer le nom de la forme et le niveau de récursivité
-        nom_forme, niveau_recursivite = cle.split('_')
-
-        # Appeler la fonction de calcul des coordonnées
-        calculer_coordonnees_fractale(nom_forme, niveau_recursivite, d[cle])
-
-# Exemple d'utilisation
-dictionnaire_figures = {
-
-}
-
-parcourir_dictionnaire(dictionnaire_figures)
